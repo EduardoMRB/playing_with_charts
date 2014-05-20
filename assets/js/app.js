08dea306c93109af085c101af2f8a9993133ef64
Eduardo Borges <eduardomrb@gmail.com>
2014-05-19 23:46:21 -0400

    define app's route provider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
 var phoneCount = angular.module('phoneCount', ['ngRoute']);
+
+phoneCount.config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.
+      when('/add', {
+        templateUrl: 'partials/add.html',
+        controller: 'AddCtrl'
+      }).
+      when('/chart', {
+        templateUrl: 'partials/chart.html',
+        controller: 'ChartCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+phoneCount.controller('AddCtrl', ['$scope', '$routeParams', 
+  function ($scope, $routeParams) {
+        
+  }]);
