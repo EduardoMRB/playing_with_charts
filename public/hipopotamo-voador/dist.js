c7f1c919808712b2e1414138c3a1a394e8a545cf
Eduardo Borges <eduardomrb@gmail.com>
2014-05-25 13:21:35 -0400

    finaliza trabalho

--- public/hipopotamo-voador/dist.js
+++ public/hipopotamo-voador/dist.js
@@ -27,10 +27,44 @@ phoneCount.controller('AddCtrl', ['$scope', '$routeParams',
   function ($scope, $routeParams) {
     $scope.events = [];
 
-    $scope.addEvent = function (quantity, date) {
-      $scope.events.push({ 
-        quantity: quantity,
-        date: date
+    function isQuantityValid (quantity) {
+      return /\d+/.test(quantity);
+    }
+
+    function isDateValid (date) {
+      return /(\d{4})-(\d{2})-(\d{2})/.test(date);
+    }
+
+    function clearScopes () {
+      $scope.quantity = '';
+      $scope.date = '';
+      $scope.error = '';
+    }
+
+    function dateExists(date) {
+      var index;
+      $scope.events.forEach(function (event, i) {
+        if (event.date == date) index = i;
       });
+
+      return index;
+    }
+
+    $scope.addEvent = function (quantity, date) {
+      if (isQuantityValid(quantity) && isDateValid(date)) {
+        var index = dateExists(date);
+
+        if (index != undefined) {
+          $scope.events[index].quantity += quantity;
+        } else {
+          $scope.events.push({ 
+            quantity: quantity,
+            date: date
+          });
+        }
+        clearScopes();
+      } else {
+        $scope.error = 'Dados inválidos, verifique os campos do formulário';
+      }
     };
   }]);
